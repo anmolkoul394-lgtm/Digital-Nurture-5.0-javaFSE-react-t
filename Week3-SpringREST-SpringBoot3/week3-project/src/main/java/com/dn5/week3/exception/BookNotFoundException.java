6e1dff3dbfcbe3bb51350463b104484b0227a7ad
anmolkoul394-lgtm <anmolkoul394@gmail.com>
2026-07-12 14:32:45 +0530

    Delete Week3-SpringREST-SpringBoot3/week3-project directory

--- Week3-SpringREST-SpringBoot3/week3-project/src/main/java/com/dn5/week3/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dn5.week3.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(Long id) {
-        super("Book not found with id: " + id);
-    }
-}
--- Week3-SpringREST-SpringBoot3/week3-project/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO books (title, author, isbn, price, stock, version) VALUES ('Clean Code', 'Robert C. Martin', '9780132350884', 899.0, 12, 0);
-INSERT INTO books (title, author, isbn, price, stock, version) VALUES ('Effective Java', 'Joshua Bloch', '9780134685991', 999.0, 8, 0);
-INSERT INTO books (title, author, isbn, price, stock, version) VALUES ('Spring in Action', 'Craig Walls', '9781617294945', 1099.0, 5, 0);
